9e7286277cc2dffadcd3d24e8e9c8d6226eafcfe
irvinakocan <44883640+irvinakocan@users.noreply.github.com>
2023-07-04 21:56:39 +0200

    Implementirana funkcija za sortiranje rijeci

--- Lab2_z5/Program.cs
+++ Lab2_z5/Program.cs
@@ -4,9 +4,45 @@ namespace Lab2_z5
 {
     class Program
     {
+        static string SortirajRijec(string s)
+        {
+            if (s.Length < 2)
+                return s;
+
+            char[] slova = new char[s.Length];
+            for (int i = 0; i < slova.Length; i++)
+                slova[i] = s[i];
+
+            for(int i = 0;i < slova.Length - 1; i++)
+            {
+                for(int j = i + 1; j < slova.Length; j++)
+                {
+                    if (slova[j] < slova[i])
+                    {
+                        char pomocna = slova[j];
+                        slova[j] = slova[i];
+                        slova[i] = pomocna;
+                    }
+                }
+            }
+
+            return string.Concat(slova);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Unesite broj rijeci: ");
+            string ulaz = Console.ReadLine();
+            int n = Int32.Parse(ulaz);
+
+            Console.WriteLine("Unesite rijeci: ");
+            string[] rijeci = new string[n];
+            for(int i = 0; i < n; i++)
+            {
+                Console.Write("{0}: ", i + 1);
+                rijeci[i] = Console.ReadLine();
+            }
+
         }
     }
 }
